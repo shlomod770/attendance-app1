05ddd323f32ad16e138b3105cd82771ebab157a4
shlomod770 <chabad.sofia@gmail.com>
2026-08-09 12:39:29 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,4 @@
-<!-- Firebase SDKs (Compat version) -->
-<script src="https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore-compat.js"></script>
-
-<script>
- const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyCurmPXzbz5lqW6VvxTWosY4RPzEUim_po",
   authDomain: "attendance-8e594.firebaseapp.com",
   projectId: "attendance-8e594",
@@ -12,7 +7,5 @@
   appId: "1:130707494835:web:a7b733cafafefcefd19dc6"
 };
 
-  // אתחול Firebase
-  firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore();
-</script>
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
